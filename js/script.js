9ee926504343a6e482f6cc1e4dcb63fdad12d2f9
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-08-03 22:31:08 +0530

    huge changed at mediaquerries css

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 // }
 
 
-document.getElementById('theme-button').addEventListener('click', changeTheme)
+document.getElementsByClassName('theme-button')[0].addEventListener('click', changeTheme)
 
 function changeTheme() {
   document.body.classList.toggle('light');
@@ -23,3 +23,21 @@ const mainStream = document.getElementById('main-stream');
 
 mainStream.style.height = `${educationDetailsHeight / 16}rem`
 console.log(`${educationDetailsHeight}`);
+
+
+// ____________________________________________________________________
+
+
+const hamburgerButton = document.querySelector('.hamburger');
+const navItem = document.querySelector('.nav-item');
+const themeButton = document.querySelector('.theme-button');
+
+hamburgerButton.addEventListener('click', openNavbar)
+
+function openNavbar() {
+  navItem.classList.toggle('open-navbar');
+  themeButton.classList.toggle('open-navbar');
+}
+
+
+// ____________________________________________________________________
