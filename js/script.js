f5ddd1a2dae9ab6aaa35101999d612355a52e7d5
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-09-21 14:27:19 +0530

    Drastic change #4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+const carousel = document.querySelector('.carousel');
+const button = carousel.querySelectorAll('button');
+const slide = carousel.querySelector('.slide')
+
+button.forEach(btn => {
+  btn.addEventListener('click', () => {
+    let allSlide = [...slide.children]
+    let activeIndex = [...slide.children].indexOf(slide.querySelector('[data-active = true]'))
+    let buttonType = btn.className
+    let nextIndex = activeIndex
+    
+    if (buttonType == 'next') {
+      nextIndex = activeIndex + 1
+    }
+    else nextIndex = activeIndex - 1
+    
+    if (nextIndex < 0) nextIndex = slide.children.length - 1
+    if (nextIndex >= slide.children.length) nextIndex = 0
+    
+    allSlide[nextIndex].dataset.active = true
+    delete allSlide[activeIndex].dataset.active
+    
+  })
+})
+
+setInterval(autoSlide, 5000)
+
+function autoSlide() {
+  let allSlide = [...slide.children]
+  let activeIndex = [...slide.children].indexOf(slide.querySelector('[data-active = true]'))
+  let nextIndex = activeIndex + 1
+
+  if (nextIndex > activeIndex) {
+    if (nextIndex < 0) nextIndex = slide.children.length - 1
+    if (nextIndex >= slide.children.length) nextIndex = 0
+
+    allSlide[nextIndex].dataset.active = true
+    delete allSlide[activeIndex].dataset.active
+  }
+
+}
+
 
 // __________________________________________________________________
 
